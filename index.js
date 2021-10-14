7f424d6ccef42b4052df356745fa8ec52a312a4d
unknown <iinf18.bsantiagot@itesco.edu.mx>
2021-10-14 04:33:54 -0500

    conexionconmongo

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const routerApi = require('./routes')
 const app = express()
-const port = 3000
+const { config } = require('./config/index')
+
+const port = config.port
+
+const mongoose = require('mongoose')
+const USER = encodeURIComponent(config.dbUser)
+const PASSWORD = encodeURIComponent(config.dbPassword)
+const DB_NAME = encodeURIComponent(config.dbName)
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`
+
+
+console.log('*******************************')
+console.log(MONGO_URI)
+mongoose.connect(MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true}
+).then(() => console.log('Existe conexión ahora')).catch(e=>console.log(e))
+
 
 app.get('/', (req, res) => {
   res.send('Holiwis! bienvenido a node y express js')
