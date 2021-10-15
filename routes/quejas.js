e587cffa1d35f5f3b7e46a59795949cb1d2aa845
unknown <iinf18.bsantiagot@itesco.edu.mx>
2021-10-15 17:57:08 -0500

    correcciónjjk

--- routes/quejas.js
+++ routes/quejas.js
@@ -5,8 +5,7 @@ const Quejas = require('../models/quejas')
 router.get('/', (req, res) =>{
      try{
           res.json({
-               name: 'Aquí podrá subir su queja',
-               valor: 3
+               name: 'Escriba el nombre de su queja'
           })
      }catch(error){
           console.log(`Error: ${error}`)
