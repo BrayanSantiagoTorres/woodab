5dc6b6b6a3ebac525877d18969b9812cff5358f2
unknown <iinf18.bsantiagot@itesco.edu.mx>
2021-10-15 17:53:36 -0500

    corrección

--- routes/muebles.js
+++ routes/muebles.js
@@ -6,19 +6,23 @@ router.get('/', (req, res) => {
   res.json([
     {
       name: 'mueble 1',
-      price: 800
+      price: 800,
+      num: 4
     },
     {
       name: 'mueble 2',
-      price: 1400
+      price: 1400,
+      num: 6
     },
     {
       name: 'mueble 3',
-      price: 2900
+      price: 2900,
+      num: 2
     },
     {
       name: 'mueble 4',
-      price: 5000
+      price: 5000,
+      num: 6
     }
   ])
 })
@@ -27,7 +31,8 @@ router.get('/filter', (req, res) => {
   res.json(
     {
       name: 'mueble 3',
-      price: 2900
+      price: 2900,
+      num: 2
     })
 })
 
