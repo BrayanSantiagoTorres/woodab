23ab50490f1df16682a70d7a24811ad8fc39a47b
unknown <iinf18.bsantiagot@itesco.edu.mx>
2021-11-08 18:50:05 -0600

    wed

--- routes/auth.js
+++ routes/auth.js
@@ -5,15 +5,15 @@ const jwt = require('jsonwebtoken')
 const router = express.Router()
 
 router.post('/login',
-  passport.authenticate('local', {session:false}),
+  passport.authenticate('local', {session:false}), //el usuario se logeaaaa
   async (req, res, next)=>{
     try{
       const user = req.user
-      const payload = {
+      const payload = { //se pide datos al usuario id y rol
         sub: user.id,
         role: user.rol
       }
-
+      //se genera un token unico con ese usuario
       const token = jwt.sign(payload, '6de389ed5da80327ab292db7c3f9f3cce1c1d343b562f8c9fa597f35bd85d8bc') //los datos se unen con la clave secreta
       res.json({ //se pide el user y el token
         user,
